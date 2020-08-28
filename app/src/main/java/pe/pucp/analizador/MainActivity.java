4a9d095282de4115c40c879dc0a4bf99a00faf6f
ergio2000 <ergio2000@gmail.com>
2020-08-27 22:05:57 -0500

    SIMPLIFICACION DE TOMAR FOTO FUNCIONAL (TOMA FOTO, DEVUELVE FOTO, ADECUA TAMAÑO Y LO MUESTRA) EN FRAGMENTO HOME SELECCION DE FOTO FUNCIONAL (SELECCIONA FOTO, DEVUELVE FOTO, ADECUA TAMAÑO Y LO MUESTRA)

--- app/src/main/java/pe/pucp/analizador/MainActivity.java
+++ app/src/main/java/pe/pucp/analizador/MainActivity.java
@@ -31,13 +31,17 @@ import android.widget.ImageView;
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-
-    private final int PICTURE_TAKEN_FROM_CAMERA = 1;
-    private final int PICTURE_TAKEN_FROM_GALLERY = 2;
+//region MANEJO DE CAMARA
+    //codigo de respuesta
+    final int PICTURE_TAKEN=1;
+    //archivo de salida para tomar foto
     private File outFile = null;
+//endregion
 
+    //auxiliar para visualizacion de imagen
     private ImageView pictureZone = null;
 
+    //on create
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,9 +49,9 @@ public class MainActivity extends AppCompatActivity {
 
         pictureZone = (ImageView)findViewById(R.id.ivimgsel);
 
-        //verifica permisos de camara y acceso a disco
-        //verificaPermisoCamara();
+        //verifica permisos de camara, y acceso a disco (camara/galeria)
         verificaPermisos();
+
     }
 
 
@@ -59,22 +63,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateOptionsMenu(menu);
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.mnuprincipal, menu );
-
-
-
         return true;
     }
 
 
-
+    //manejo de elementos de menu
     public boolean onOptionsItemSelected(MenuItem item) {
         // Find which Menu Item has been selected
         switch (item.getItemId()) {
             // Check for each known Menu Item
             case (R.id.action_tomarfoto):
-                Log.wtf("mainactivity","tomarfoto ini");
+                //solicita tomar foto
                 startCamera();
-                Log.wtf("mainactivity","tomarfoto fin");
                 return true;
                 // Pass on any unhandled Menu Items to super.onOptionsItemSelected
                 // This is required to ensure that the up button and Fragment Menu Items
@@ -83,55 +83,37 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+//region MANEJO DE CAMARA
+
+    //inicializacion de la camara y peticion de captura de foto
     private void startCamera()
     {
-
+        try{
             Intent takePicIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-            //We prepare the intent to store the taken picture
-            try{
-                //File outputDir = ToolBox.storage_getExternalPublicFolder(Environment.DIRECTORY_PICTURES, "testApp", true);
-                File outputDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-                //outFile = ToolBox.storage_createUniqueFileName("cameraPic", ".jpg", outputDir);
-                outFile= new File(outputDir, "cameraPic.jpg");
-                takePicIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(outFile));
-
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-            try{
-                Log.wtf("startcamera2","inicio activity");
-                startActivityForResult(takePicIntent, PICTURE_TAKEN_FROM_CAMERA);
-            }catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-
+            File outputDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            outFile= new File(outputDir, "cameraPic.jpg");
+            takePicIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(outFile));
+            startActivityForResult(takePicIntent, PICTURE_TAKEN);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        Log.wtf("onActivityResult","inicio");
         Bitmap takenPictureData = null;
 
-        switch(requestCode){
+        switch(requestCode) {
 
-            case PICTURE_TAKEN_FROM_CAMERA:
-                if(resultCode==Activity.RESULT_OK) {
+            case PICTURE_TAKEN:
+                if (resultCode == Activity.RESULT_OK) {
                     takenPictureData = handleResultFromCamera(data);
                 }
                 break;
-            case PICTURE_TAKEN_FROM_GALLERY:
-                if(resultCode==Activity.RESULT_OK) {
-                    //todo
-                    // takenPictureData = handleResultFromChooser(data);
-                }
-                break;
         }
-
         //And show the result in the image view.
         if(takenPictureData!=null){
 
@@ -142,8 +124,8 @@ public class MainActivity extends AppCompatActivity {
             //datos de vista
             int ivWidth = pictureZone.getWidth();
             int ivHeight = pictureZone.getHeight();
-            String msg=" ("+String.valueOf(currentBitmapWidth)+","+String.valueOf(currentBitmapHeight)+")   ,    ("+String.valueOf(ivWidth)+","+String.valueOf(ivHeight)+")";
-            Log.wtf("imagenorigendestino",msg);
+            //String msg=" ("+String.valueOf(currentBitmapWidth)+","+String.valueOf(currentBitmapHeight)+")   ,    ("+String.valueOf(ivWidth)+","+String.valueOf(ivHeight)+")";
+            //Log.wtf("imagenorigendestino",msg);
             //escalamiento
             int newWidth = ivWidth;
             int newHeight = (int) Math.floor((double) currentBitmapHeight *( (double) newWidth / (double) currentBitmapWidth));
@@ -155,7 +137,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Bitmap handleResultFromCamera(Intent data){
         Bitmap takenPictureData = null;
-        Log.wtf("handleResultFromCamera","inicio");
+        //Log.wtf("handleResultFromCamera","inicio");
 
         if(data!=null){
             //Android sets the picture in extra data.
@@ -168,56 +150,26 @@ public class MainActivity extends AppCompatActivity {
             //from the output.
             try{
                 //takenPictureData = ToolBox.media_getBitmapFromFile(outFile);
-                //String na= String.valueOf(Uri.fromFile(outFile));
                 String na=outFile.getPath();
-                Log.wtf("na",na);
-                Log.wtf("ruta",outFile.getPath());
-                Log.wtf("nombre",outFile.getName());
+                //Log.wtf("na",na);
                 takenPictureData = BitmapFactory.decodeFile(na);
-
             }catch(Exception e){
                 e.printStackTrace();
             }
         }
-
-
         return takenPictureData;
     }
 
 
-
+    //utilitario de verificacion de permisos
     private void verificaPermisos()
     {
         int permissionCode=1;
-        /*
-        String CAMERA_PERMISSION = Manifest.permission.CAMERA;
-        String READ_EXTERNAL_STORAGE_PERMISSION = Manifest.permission.READ_EXTERNAL_STORAGE; 
-        String WRITE_EXTERNAL_STORAGE_PERMISSION = Manifest.permission.WRITE_EXTERNAL_STORAGE;
-        String[] PERMISSIONS = {CAMERA_PERMISSION, READ_EXTERNAL_STORAGE_PERMISSION, WRITE_EXTERNAL_STORAGE_PERMISSION};
-*/
         String[] PERMISSIONS = {Manifest.permission.CAMERA,
                 Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE};
         ActivityCompat.requestPermissions((Activity) this, PERMISSIONS, permissionCode);
     }
 
-    private void verificaPermisoCamara()
-    {
-        if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.CAMERA ) != PackageManager.PERMISSION_GRANTED ) {
-            final int MY_CAMERA_REQUEST_CODE = 100;
-            ActivityCompat.requestPermissions( this, new String[] {  Manifest.permission.CAMERA },
-                    MY_CAMERA_REQUEST_CODE );
-        }
-    }
-
-    /** Check if this device has a camera */
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
-        }
-    }
+//endregion
 }
--- app/src/main/java/pe/pucp/analizador/home.java
+++ app/src/main/java/pe/pucp/analizador/home.java
@@ -1,64 +1,166 @@
 package pe.pucp.analizador;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link home#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.File;
+
 public class home extends Fragment {
+//region MANEJO DE CAMARA
+    //codigo de respuesta
+    final int PICTURE_TAKEN=1;
+//endregion
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    //auxiliar para visualizacion de imagen
+    private ImageView pictureZone = null;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public home() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment home.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static home newInstance(String param1, String param2) {
-        home fragment = new home();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //verifica permisos de camara, y acceso a disco (camara/galeria)
+        verificaPermisos();
+
+        //establece listener
+        Button btn_sel_img = root.findViewById(R.id.btnselimg);
+        btn_sel_img.setOnClickListener(new Button.OnClickListener()
+        {
+            public void onClick(View v) {
+                startGaleria();
+            }
+        });
+
+        pictureZone = root.findViewById(R.id.ivimgsel);
+
+        return  root;
     }
+
+
+//region MANEJO DE GALERIA
+
+    //inicializacion de la galeria y peticion de seleccion de imagen
+    private void startGaleria()
+    {
+        //This takes images directly from gallery
+        try
+        {
+            Intent gallerypickerIntent = new Intent(Intent.ACTION_PICK);
+            gallerypickerIntent.setType("image/*");
+            startActivityForResult(gallerypickerIntent, PICTURE_TAKEN);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap takenPictureData = null;
+
+        switch(requestCode){
+
+            case PICTURE_TAKEN:
+                if(resultCode==Activity.RESULT_OK) {
+                    takenPictureData = handleResultFromChooser(data);
+                }
+                break;
+        }
+
+        //And show the result in the image view.
+        if(takenPictureData!=null){
+
+            //adecua tamaño de imagen seleccionada a vista
+            //datos de imagen original
+            int currentBitmapWidth = takenPictureData.getWidth();
+            int currentBitmapHeight = takenPictureData.getHeight();
+            //datos de vista
+            int ivWidth = pictureZone.getWidth();
+            int ivHeight = pictureZone.getHeight();
+            //String msg=" ("+String.valueOf(currentBitmapWidth)+","+String.valueOf(currentBitmapHeight)+")   ,    ("+String.valueOf(ivWidth)+","+String.valueOf(ivHeight)+")";
+            //Log.wtf("imagenorigendestino",msg);
+            //escalamiento
+            int newWidth = ivWidth;
+            int newHeight = (int) Math.floor((double) currentBitmapHeight *( (double) newWidth / (double) currentBitmapWidth));
+            Bitmap newbitMap = Bitmap.createScaledBitmap(takenPictureData, newWidth, newHeight, true);
+            //muestra en la vista
+            pictureZone.setImageBitmap(newbitMap);
+        }
+    }
+
+    private Bitmap handleResultFromChooser(Intent data){
+        Bitmap takenPictureData = null;
+
+        Uri photoUri = data.getData();
+        if (photoUri != null){
+            try {
+
+                //We get the file path from the media info returned by the content resolver
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                Cursor cursor = getActivity().getApplicationContext().getContentResolver().query(photoUri, filePathColumn, null, null, null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String filePath = cursor.getString(columnIndex);
+                cursor.close();
+
+                String na=filePath;
+                //Log.wtf("na",na);
+                takenPictureData = BitmapFactory.decodeFile(na);
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        return takenPictureData;
+    }
+
+    //utilitario de verificacion de permisos
+    private void verificaPermisos()
+    {
+        int permissionCode=1;
+        String[] PERMISSIONS = {Manifest.permission.CAMERA,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        ActivityCompat.requestPermissions((Activity) getActivity(), PERMISSIONS, permissionCode);
+    }
+
+//endregion
+
+
+
+
 }
